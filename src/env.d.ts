f9504cc157aa5582d1be83b53cd88a55bf02086b
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2023-11-04 23:44:01 -0500

    integrate in lucia for login stuff - logout broke

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+/// <reference types="lucia" />
+declare namespace Lucia {
+	// ...
+}
+
+/// <reference types="astro/client" />
+declare namespace App {
+	interface Locals {
+		auth: import("lucia").AuthRequest;
+	}
+}
---
+++ src/lib/lucia.ts
@@ -0,0 +1,54 @@
+import { lucia } from "lucia";
+import { astro } from "lucia/middleware";
+import { mysql2 } from "@lucia-auth/adapter-mysql";
+import mysql from "mysql2/promise";
+import { github } from "@lucia-auth/oauth/providers";
+
+// expect error (see next section)
+/* export const auth = lucia({
+	env: import.meta.env.DEV ? "DEV" : "PROD",
+	middleware: astro()
+});
+
+export type Auth = typeof auth; */
+
+const connectionPool = mysql.createPool({
+	host: import.meta.env.DB_HOST, 
+    user: import.meta.env.DB_USER, 
+    database: import.meta.env.DB_DATABASE,
+    password: import.meta.env.DB_PASSWORD,
+    connectionLimit: import.meta.env.DB_CONNECTIONLIMIT,
+});
+
+export const auth = lucia({
+	adapter: mysql2(connectionPool, {
+		user: "auth_user",
+		key: "user_key",
+		session: "user_session"
+	}),
+	env: import.meta.env.DEV ? "DEV" : "PROD",
+	middleware: astro(),
+
+	getUserAttributes: (data) => {
+		return {
+			githubUsername: data.username
+		};
+	}
+});
+
+export const githubAuth = github(auth, {
+	clientId: import.meta.env.GITHUB_CLIENT_ID,
+	clientSecret: import.meta.env.GITHUB_CLIENT_SECRET
+});
+
+export type Auth = typeof auth;
+
+/* const auth = lucia({
+	env: import.meta.env.DEV ? "DEV" : "PROD",
+	middleware: astro(),
+	adapter: mysql2(connectionPool, {
+		user: "auth_user",
+		key: "user_key",
+		session: "user_session"
+	})
+}); */
