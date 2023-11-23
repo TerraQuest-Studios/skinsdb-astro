f5cf757afaa2a30739712aed797006596af20149
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2023-11-22 21:44:53 -0600

    fix https://github.com/TerraQuest-Studios/skinsdb-astro/issues/3

---
+++ src/lib/fields/field_display_name.js
@@ -0,0 +1,5 @@
+import {field_core_text} from './field_core_text.js';
+
+export class field_display_name extends field_core_text {
+
+}
--- src/lib/user.js
+++ src/lib/user.js
@@ -10,12 +10,12 @@ export class User {
         return (async () => {
             let userInfo = await db.query('SELECT u.*, uf.*, ug.id as groupid, ug.name as groupname FROM auth_user u JOIN user_fields uf ON u.id=uf.id JOIN user_groups ug ON ug.id=uf.group WHERE u.id = ?', [this.id]);
             //console.log(userInfo);
-            this.displayName = userInfo[0].display_name;
+            this.displayName = userInfo[0]?.display_name;
             this.group = {
-                "id": userInfo[0].groupid,
-                "name": userInfo[0].groupname,
+                "id": userInfo[0]?.groupid,
+                "name": userInfo[0]?.groupname,
             }
-            this.state = userInfo[0].state;
+            this.state = userInfo[0]?.state ?? -1;
             
             return this;
         })();
